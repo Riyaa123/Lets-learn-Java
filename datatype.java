9134e42dd60a822548e4c4ac96e7e03f0e3321c4
Riyaa123 <77445042+Riyaa123@users.noreply.github.com>
2021-07-08 18:42:50 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,25 @@
+public class datatype {
+    public static void main(String[] arg){
+        byte b=120;
+        short s=1234;
+        int i=2345678;
+        long l=7777777789687L;
+        float f=123.56f;
+        double d=54.8686399d;
+        boolean boo=true;
+        char c='Z';
+        String str="My Message";
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(boo);
+        System.out.println(c);
+        System.out.println(str); 
+
+
+    }
+    
+}
